861fe9c720248f17e8f2c276062ae4bd31ac2c22
Navneet Adlak <aniketadlak9@gmail.com>
2024-10-19 21:49:26 +0530

    error handlling

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
- const express = require("express");
+const express = require("express");
 
- const app = express();
- const { adminAuth } = require("./src/middlewares") 
+const app = express();
+//  const { adminAuth, userAuth } = require("./src/middlewares/auth")
 
 // handle auth middleware for all GET, POST, PATCH, DELETE request
-app.use('/admin', adminAuth)
+// app.use('/admin', adminAuth)
 
- app.get('/user', (req, res) =>{
-  res.send("user data sent")
- })
+app.get("/getUserData", (req, res) => {
+  try {
+    // Logic of DB call and get user data
+    throw new Error("hkfhakhf");
+    res.send("user Data sent");
+  } catch (err) {
+    res.status(500).send("Some Error Contact Support Team");
+  }
+});
 
- app.get('/admin/getAllData', (req, res) =>{
-   res.send("All Data Sent")
- });
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    // log your error message
+    res.status(500).send("something went wrong");
+  }
+});
 
- app.get('/admin/deleteUser', (req, res) => {
-  res.send("Deleted a User")
-}); 
+// app.post('/user/login', (req, res)=> {
+//   res.send("user logged in successfully")
+// })
 
- app.listen(3000, ()=>{
-    console.log("Server is listening on port 3000");
- });
+//  app.get('/user/data', userAuth, (req, res) =>{
+//   res.send("user data sent")
+//  })
+
+//  app.get('/admin/getAllData', (req, res) =>{
+//    res.send("All Data Sent")
+//  });
+
+//  app.get('/admin/deleteUser', (req, res) => {
+//   res.send("Deleted a User")
+// });
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,4 +1,4 @@
-export const adminAuth = (req, res, next)=>{
+const adminAuth = (req, res, next)=>{
     console.log("Admin auth is getting Checked!!");
     const token = "xyz";
     const isAuthorize = token === "xyz";
@@ -8,4 +8,20 @@ export const adminAuth = (req, res, next)=>{
     }else{
       next();
     }
+  }
+
+  const userAuth = (req, res, next)=>{
+    console.log("User auth is getting Checked!!");
+    const token = "xyz";
+    const isAuthorize = token === "xyz";
+    
+    if(!isAuthorize){
+      res.status(401).send("Unautorized request");
+    }else{
+      next();
+    }
+  }
+
+  module.exports = {
+    adminAuth, userAuth
   }
