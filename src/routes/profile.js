de1953870b563ef1f03c0869b4b4aa8c5cc4c99c
Navneet Adlak <aniketadlak9@gmail.com>
2025-02-03 22:35:50 +0530

    added logout + edit profile api

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const profileRouter = express.Router();
 const { userAuth } = require("../middlewares/auth")
+const {validateEditProfileData} = require("../utils/validation")
 
-
-profileRouter.get("/profile", userAuth, async (req, res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
     const user = req.user;
     res.send(user);
@@ -11,5 +11,22 @@ profileRouter.get("/profile", userAuth, async (req, res) => {
     res.status(400).send("Error :" + err.message);
   }
 });
+
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+  try{
+    if(!validateEditProfileData(req)){
+      throw new Error("Invalid Edit Request")
+    }
+    const loggedInUser = req.user;
+    console.log(loggedInUser)
+    Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]))
+    await loggedInUser.save();
+    console.log(loggedInUser)
+    res.send("Profile Updaaed Successfully")
+  }catch (err){
+    res.status(400).send("ERROR : "+ err.message);
+  }
+
+})
 
 module.exports = profileRouter;
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -2,19 +2,37 @@ const validator = require("validator");
 const { validate } = require("../models/user");
 
 const validateSignUpData = (req) => {
-    const {firstName, lastName, emailId, password } = req.body;
+  const { firstName, lastName, emailId, password } = req.body;
 
-    if(!firstName || !lastName){
-        throw new Error("Name is not valid")
-    }else if(firstName.length < 4 || firstName.length > 50){
-        throw new Error("firstName should be 4-50 characters");
-    }else if(!validator.isEmail(emailId)){
-        throw new Error("Email is not valid")
-    }else if(!validator.isStrongPassword(password)){
-        throw new Error("Please enter a strong password")
-    }
+  if (!firstName || !lastName) {
+    throw new Error("Name is not valid");
+  } else if (firstName.length < 4 || firstName.length > 50) {
+    throw new Error("firstName should be 4-50 characters");
+  } else if (!validator.isEmail(emailId)) {
+    throw new Error("Email is not valid");
+  } else if (!validator.isStrongPassword(password)) {
+    throw new Error("Please enter a strong password");
+  }
+};
+
+const validateEditProfileData = (req) => {
+  const allowedEditFields = [
+    "firstName",
+    "lastName",
+    "emailId",
+    "photoUrl",
+    "gender",
+    "age",
+    "about",
+    "skills",
+  ];
+
+  const isEditAllowed = Object.keys(req.body).every((field) =>
+    allowedEditFields.includes(field)
+  );
+  return isEditAllowed;
 };
 
 module.exports = {
-    validateSignUpData,
-}
+  validateSignUpData, validateEditProfileData
+};
